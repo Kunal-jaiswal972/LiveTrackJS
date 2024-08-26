da1cde1ec4892341638051f295e2b3023d115973
kunal <kishujaiswal3@gmail.com>
2024-08-26 20:16:40 +0530

    Fixed errors

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { generateApiKey } from "../controllers/user.controller.js";
+import {
+  getSitesForUser,
+  getActivitiesForSite,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/generate-api-key", verifyToken, generateApiKey);
+router.get("/sites", verifyToken, getSitesForUser);
+router.get("/sites/:siteId/activities", verifyToken, getActivitiesForSite);
 
 export default router;
